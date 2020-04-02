041c5955bdea79b44a2df1f2840aec8647115902
petreasil <42999818+petreasil@users.noreply.github.com>
2020-04-02 11:48:56 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Container from "components/Container";
+
+const Header = () => {
+  return (
+    <header>
+      <Container type="content">
+        <p>Covid-19 World Map</p>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
